f3bdd4a366ad34817472682ca060b5587053e118
melaniesdavis <melaniesdavis@hotmail.com>
2017-02-08 15:46:49 -0500

    Added Series class

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -10,10 +10,18 @@ namespace ComicBookGalleryModel.Models
     {
         // ID, ComicBookId, ComicBookID
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public Series Series { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public decimal? AverageRating { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Series?.Title} #{IssueNumber}";
+            }
+        }
     }
 }
